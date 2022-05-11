738d7ee07c5094de113cf939dbdf37b4b185f038
Diyorbek7 <jumaniyozovdiyorbek29@gmail.com>
2022-05-11 21:51:11 +0300

    log in

--- lib/src/ui/auth/login_screen.dart
+++ lib/src/ui/auth/login_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:zetgram_app/src/utils/app_color/app_color.dart';
@@ -12,6 +11,11 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _controllerEmail = TextEditingController();
+  final TextEditingController _controllerPassword = TextEditingController();
+
+  bool isHiddenPassword = true;
+
   @override
   Widget build(BuildContext context) {
     double w = Utils.getWidth(context);
@@ -19,38 +23,392 @@ class _LoginScreenState extends State<LoginScreen> {
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: AppColor.lightWhite,
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(0),
-        child: AppBar(
-          elevation: 0,
-          backgroundColor: AppColor.lightWhite,
-        ),
-      ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            height: 64 * h,
-            width: 64 * h,
-            margin: EdgeInsets.only(
-              top: 24 * h,
-            ),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              color: AppColor.blue,
-            ),
-            child: Center(
-              child: SvgPicture.asset(
-                "assets/icons/zetgram.svg",
-                height: 32 * h,
-                width: 32 * h,
+          Center(
+            child: Container(
+              height: 64 * h,
+              width: 64 * h,
+              margin: EdgeInsets.only(
+                top: 68 * h,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: AppColor.blue,
+              ),
+              child: Center(
+                child: SvgPicture.asset(
+                  "assets/icons/zetgram.svg",
+                  height: 32 * h,
+                  width: 32 * h,
+                ),
               ),
             ),
           ),
+          SizedBox(
+            height: 40 * h,
+          ),
+          Row(
+            children: [
+              Container(
+                margin: EdgeInsets.only(
+                  left: 16 * w,
+                ),
+                padding: EdgeInsets.symmetric(
+                  horizontal: 12 * w,
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      "Log In",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 20 * h,
+                        fontFamily: AppColor.fontNunitoSans,
+                        height: 33 / 20 * h,
+                        color: AppColor.dark,
+                      ),
+                    ),
+                    Container(
+                      height: 6 * h,
+                      width: 6 * h,
+                      margin: EdgeInsets.only(
+                        top: 4 * h,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(3),
+                        color: AppColor.blue,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  left: 22 * w,
+                ),
+                padding: EdgeInsets.symmetric(
+                  horizontal: 12 * w,
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 20 * h,
+                        fontFamily: AppColor.fontNunitoSans,
+                        height: 27 / 20 * h,
+                        color: AppColor.grey,
+                      ),
+                    ),
+                    Container(
+                      height: 6 * h,
+                      width: 6 * h,
+                      margin: EdgeInsets.only(
+                        top: 4 * h,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(3),
+                        color: Colors.transparent,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
           Expanded(
-            child: ListView(),
+            child: ListView(
+              padding: EdgeInsets.symmetric(
+                horizontal: 25 * w,
+              ),
+              children: [
+                SizedBox(
+                  height: 41 * h,
+                ),
+                Text(
+                  "Username or Email",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16 * h,
+                    fontFamily: AppColor.fontNunitoSans,
+                    height: 27 / 16 * h,
+                    color: AppColor.mediumDark,
+                  ),
+                ),
+                Container(
+                  width: width,
+                  margin: EdgeInsets.only(
+                    top: 15 * h,
+                  ),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 20 * w,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(27.5),
+                    color: AppColor.white,
+                    boxShadow: [
+                      BoxShadow(
+                        offset: const Offset(
+                          0,
+                          10,
+                        ),
+                        blurRadius: 75,
+                        color: const Color(0xFF939393).withOpacity(0.1),
+                      ),
+                    ],
+                  ),
+                  child: TextField(
+                    controller: _controllerEmail,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Enter your username or email",
+                      hintStyle: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 14 * h,
+                        fontFamily: AppColor.fontNunitoSans,
+                        height: 25 / 14 * h,
+                        color: AppColor.grey.withOpacity(0.8),
+                      ),
+                    ),
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16 * h,
+                      fontFamily: AppColor.fontNunitoSans,
+                      height: 25 / 16 * h,
+                      color: AppColor.dark,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 35 * h,
+                ),
+                Text(
+                  "Password",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16 * h,
+                    fontFamily: AppColor.fontNunitoSans,
+                    height: 27 / 16 * h,
+                    color: AppColor.mediumDark,
+                  ),
+                ),
+                Container(
+                  width: width,
+                  margin: EdgeInsets.only(
+                    top: 15 * h,
+                  ),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 20 * w,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(27.5),
+                    color: AppColor.white,
+                    boxShadow: [
+                      BoxShadow(
+                        offset: const Offset(
+                          0,
+                          10,
+                        ),
+                        blurRadius: 75,
+                        color: const Color(0xFF939393).withOpacity(0.1),
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          obscureText: isHiddenPassword,
+                          controller: _controllerPassword,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Enter your password",
+                            hintStyle: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 14 * h,
+                              fontFamily: AppColor.fontNunitoSans,
+                              height: 25 / 14 * h,
+                              color: AppColor.grey.withOpacity(0.8),
+                            ),
+                          ),
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 16 * h,
+                            fontFamily: AppColor.fontNunitoSans,
+                            height: 25 / 16 * h,
+                            color: AppColor.dark,
+                          ),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: _togglePassword,
+                        child: Container(
+                          height: 28 * h,
+                          width: 28 * h,
+                          margin: EdgeInsets.only(
+                            left: 6 * w,
+                          ),
+                          child: Center(
+                            child: isHiddenPassword ? SvgPicture.asset(
+                              "assets/icons/eye-off.svg",
+                              height: 24 * h,
+                              width: 24 * h,
+                            ) : SvgPicture.asset(
+                              "assets/icons/eye.svg",
+                              height: 24 * h,
+                              width: 24 * h,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 25 * h,
+                ),
+                Row(
+                  children: [
+                    const Expanded(child: Text("")),
+                    Text(
+                      "Forgot Password?",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 14 * h,
+                        fontFamily: AppColor.fontNunitoSans,
+                        height: 25 / 14 * h,
+                        color: AppColor.dark.withOpacity(0.8),
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  height: 57 * h,
+                  margin: EdgeInsets.only(
+                    top: 30 * h,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(28.5),
+                    color: AppColor.blue,
+                    boxShadow: [
+                      BoxShadow(
+                        offset: const Offset(
+                          0,
+                          6,
+                        ),
+                        blurRadius: 75,
+                        color: const Color(0xFF645757).withOpacity(0.05),
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 18 * h,
+                        fontFamily: AppColor.fontNunitoSans,
+                        height: 25 / 18 * h,
+                        color: AppColor.white,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                    top: 33 * h,
+                    left: 8 * w,
+                    right: 8 * w,
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 94 * w,
+                        height: 1 * h,
+                        color: AppColor.grey.withOpacity(0.6),
+                      ),
+                      Expanded(
+                        child: Container(
+                          margin: EdgeInsets.only(left: 20*w,),
+                          child: Text(
+                            "Or login with",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 14 * h,
+                              fontFamily: AppColor.fontNunitoSans,
+                              height: 25 / 14 * h,
+                              color: AppColor.dark.withOpacity(0.8),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 94 * w,
+                        height: 1 * h,
+                        color: AppColor.grey,
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 57 * h,
+                  margin: EdgeInsets.only(
+                    top: 20 * h,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(28.5),
+                    color: AppColor.white,
+                    boxShadow: [
+                      BoxShadow(
+                        offset: const Offset(
+                          0,
+                          6,
+                        ),
+                        blurRadius: 75,
+                        color: const Color(0xFF645757).withOpacity(0.15),
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Row(
+                      children: [
+                        const Spacer(),
+                        Image.asset(
+                          "assets/images/google.png",
+                          height: 22 * h,
+                          width: 22 * h,
+                        ),
+                        SizedBox(
+                          width: 15 * w,
+                        ),
+                        Text(
+                          "Google",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 18 * h,
+                            fontFamily: AppColor.fontNunitoSans,
+                            height: 25 / 18 * h,
+                            color: AppColor.dark,
+                          ),
+                        ),
+                        const Spacer(),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
     );
+  }
+  void _togglePassword() {
+    setState(() {
+      isHiddenPassword = !isHiddenPassword;
+    });
   }
 }
--- lib/src/utils/app_color/app_color.dart
+++ lib/src/utils/app_color/app_color.dart
@@ -8,6 +8,7 @@ class AppColor {
   static const Color white = Color(0xFFFFFFFF);
   static const Color lightWhite = Color(0xFFFAFAFA);
   static const Color dark = Color(0xFF383838);
+  static const Color mediumDark = Color(0xFF3E3E3E);
   static const Color blue = Color(0xFF327FEB);
   static const Color grey = Color(0xFFA9A9A9);
 }
