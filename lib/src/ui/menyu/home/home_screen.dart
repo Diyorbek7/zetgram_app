5f9096f6d7413d82dd708c940ea5969bda9139fb
Diyorbek7 <jumaniyozovdiyorbek29@gmail.com>
2022-05-16 16:19:31 +0300

    verification

--- lib/src/ui/menyu/home/home_screen.dart
+++ lib/src/ui/menyu/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:zetgram_app/src/utils/app_color/app_color.dart';
@@ -48,8 +49,11 @@ class _HomeScreenState extends State<HomeScreen> {
       body: ListView(
         children: [
           SizedBox(
-            height: 92 * h,
+            height: 130 * h,
             child: ListView.builder(
+              padding: EdgeInsets.only(
+                top: 25 * h,
+              ),
               itemCount: 7,
               scrollDirection: Axis.horizontal,
               itemBuilder: (context, index) {
@@ -57,33 +61,70 @@ class _HomeScreenState extends State<HomeScreen> {
                   children: [
                     Expanded(
                       child: index == 0
-                          ? DottedBorder(
-                              strokeWidth: 3,
-                              dashPattern: const [8, 5],
-                              borderType: BorderType.RRect,
-                              radius: const Radius.circular(9),
-                              child: Container(
-                                height: 57 * h,
-                                width: 57 * h,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(9),
-                                ),
-                              ),
-                              color: AppColor.blue,
-                            )
-                          : Container(
-                              height: 57 * h,
-                              width: 57 * h,
+                          ? Container(
+                              width: 60 * w,
                               margin: EdgeInsets.only(
-                                right: 25 * w,
+                                right: 32 * w,
+                                left: 25 * w,
                               ),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(9),
-                                border: Border.all(
-                                  color: AppColor.blue,
-                                  width: 3,
+                              child: Column(
+                                children: [
+                                  SizedBox(
+                                    height: 55 * h,
+                                    child: DottedBorder(
+                                      strokeWidth: 3,
+                                      dashPattern: const [8, 5],
+                                      borderType: BorderType.RRect,
+                                      radius: const Radius.circular(9),
+                                      child: Container(
+                                        height: 55 * h,
+                                        width: 53 * h,
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(9),
+                                        ),
+                                      ),
+                                      color: AppColor.blue,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 10 * h,
+                                  ),
+                                  Text(
+                                    "Add Sto55ry",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 13 * h,
+                                      fontFamily: AppColor.fontNunitoSans,
+                                      height: 24 / 13 * h,
+                                      color: AppColor.dark,
+                                    ),
+                                    maxLines: 1,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ],
+                              ),
+                            )
+                          : Column(
+                              children: [
+                                Container(
+                                  height: 57 * h,
+                                  margin: EdgeInsets.only(
+                                    right: 25 * w,
+                                  ),
+                                  child: Container(
+                                    height: 57 * h,
+                                    width: 57 * h,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(9),
+                                      border: Border.all(
+                                        color: AppColor.blue,
+                                        width: 3,
+                                      ),
+                                    ),
+                                  ),
                                 ),
-                              ),
+                              ],
                             ),
                     ),
                   ],
