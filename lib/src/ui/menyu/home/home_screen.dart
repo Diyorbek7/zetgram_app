127bb2a0d816d5d325705a95ad0ff526aec3d5c0
Diyorbek7 <jumaniyozovdiyorbek29@gmail.com>
2022-05-25 20:49:38 +0300

    home

--- lib/src/ui/menyu/home/home_screen.dart
+++ lib/src/ui/menyu/home/home_screen.dart
@@ -1,6 +1,4 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:dotted_border/dotted_border.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:zetgram_app/src/model/story/story_model.dart';
@@ -262,11 +260,121 @@ class _HomeScreenState extends State<HomeScreen> {
                     ),
                     SvgPicture.asset(
                       "assets/icons/menu.svg",
-                      height: 24*h,
-                      width: 24*h,
+                      height: 24 * h,
+                      width: 24 * h,
                     ),
-                    SizedBox(width: 19*w,),
+                    SizedBox(
+                      width: 19 * w,
+                    ),
                   ],
+                ),
+                Container(
+                  height: 1 * h,
+                  width: MediaQuery.of(context).size.width,
+                  margin: EdgeInsets.only(
+                    top: 17 * h,
+                    left: 20 * w,
+                    right: 20 * w,
+                  ),
+                  color: AppColor.grey.withOpacity(0.2),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                    top: 20 * h,
+                    left: 15 * w,
+                    right: 15 * w,
+                  ),
+                  child: Text(
+                    "My last day for this year holiday! So excited to share my memories with you guys! 😁😍",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14 * h,
+                      fontFamily: AppColor.fontNunitoSans,
+                      height: 24 / 14 * h,
+                      color: AppColor.dark.withOpacity(0.8),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                    top: 20 * h,
+                    left: 15 * w,
+                    right: 15 * w,
+                  ),
+                  color: Colors.transparent,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset(
+                      "assets/images/img1.png",
+                      height: 170 * h,
+                      width: MediaQuery.of(context).size.width,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: EdgeInsets.only(
+                    left: 15 * w,
+                    top: 20 * h,
+                    right: 15 * w,
+                    bottom: 20*h,
+                  ),
+                  child: Row(
+                    children: [
+                      SvgPicture.asset(
+                        "assets/icons/like.svg",
+                        height: 24 * h,
+                        width: 24 * h,
+                      ),
+                      SizedBox(
+                        width: 11 * w,
+                      ),
+                      Text(
+                        "1.892",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 14 * h,
+                          fontFamily: AppColor.fontNunitoSans,
+                          height: 24 / 14 * h,
+                          color: AppColor.dark.withOpacity(0.8),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 23 * w,
+                      ),
+                      SvgPicture.asset(
+                        "assets/icons/comment.svg",
+                        height: 24 * h,
+                        width: 24 * h,
+                      ),
+                      SizedBox(
+                        width: 10 * w,
+                      ),
+                      Expanded(
+                        child: Text(
+                          "72",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 14 * h,
+                            fontFamily: AppColor.fontNunitoSans,
+                            height: 24 / 14 * h,
+                            color: AppColor.dark.withOpacity(0.8),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "2 Hours ago",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 13 * h,
+                          fontFamily: AppColor.fontNunitoSans,
+                          height: 24 / 13 * h,
+                          color: AppColor.grey,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
