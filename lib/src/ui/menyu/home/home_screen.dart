d48637763adfaafb18b43f61cedc31051a946809
Diyorbek7 <jumaniyozovdiyorbek29@gmail.com>
2022-05-25 19:26:56 +0300

    home

--- lib/src/ui/menyu/home/home_screen.dart
+++ lib/src/ui/menyu/home/home_screen.dart
@@ -79,131 +79,128 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: ListView(
         children: [
-          SizedBox(
-            height: 130 * h,
-            child: ListView.builder(
-              padding: EdgeInsets.only(
-                top: 25 * h,
-              ),
-              itemCount: data.length,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) {
-                return Column(
-                  children: [
-                    Expanded(
-                      child: index == 0
-                          ? Container(
-                              width: 57 * h,
-                              margin: EdgeInsets.only(
-                                right: 32 * w,
-                                left: 25 * w,
-                              ),
-                              child: Column(
-                                children: [
-                                  SizedBox(
-                                    height: 55 * h,
-                                    child: DottedBorder(
-                                      strokeWidth: 3,
-                                      dashPattern: const [8, 5],
-                                      borderType: BorderType.RRect,
-                                      radius: const Radius.circular(9),
-                                      child: Container(
-                                        height: 57 * h,
-                                        width: 57 * h,
-                                        decoration: BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(9),
+          Expanded(
+            child: SizedBox(
+              height: 130 * h,
+              child: ListView.builder(
+                padding: EdgeInsets.only(
+                  top: 25 * h,
+                ),
+                itemCount: data.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      Expanded(
+                        child: index == 0
+                            ? Container(
+                                width: 60 * w,
+                                margin: EdgeInsets.only(
+                                  right: 29 * w,
+                                  left: 25 * w,
+                                ),
+                                child: Column(
+                                  children: [
+                                    DottedBorder(
+                                        strokeWidth: 2.5,
+                                        dashPattern: const [8, 5],
+                                        borderType: BorderType.RRect,
+                                        radius: const Radius.circular(9),
+                                        child: Container(
+                                          height: 52 * h,
+                                          width: 52 * h,
+                                          decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(9),
+                                          ),
+                                          child: Center(
+                                            child: SvgPicture.asset(
+                                              "assets/icons/plus.svg",
+                                              height: 28 * h,
+                                              width: 28 * h,
+                                            ),
+                                          ),
                                         ),
-                                        child: Center(
-                                          child: SvgPicture.asset(
-                                            "assets/icons/plus.svg",
-                                            height: 28 * h,
-                                            width: 28 * h,
+                                        color: AppColor.blue,
+                                      ),
+                                    SizedBox(
+                                      height: 10 * h,
+                                    ),
+                                    Text(
+                                      "Add Story",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w400,
+                                        fontSize: 13 * h,
+                                        fontFamily: AppColor.fontNunitoSans,
+                                        height: 24 / 13 * h,
+                                        color: AppColor.dark,
+                                      ),
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ],
+                                ),
+                              )
+                            : Container(
+                                width: 59 * h,
+                                margin: EdgeInsets.only(
+                                  right: 25 * w,
+                                ),
+                                child: Column(
+                                  children: [
+                                    SizedBox(
+                                      height: 59 * h,
+                                      child: Container(
+                                        height: 59 * h,
+                                        width: 59 * h,
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(9),
+                                          border: Border.all(
+                                            color: AppColor.blue,
+                                            width: 3,
+                                          ),
+                                        ),
+                                        child: ClipRRect(
+                                          borderRadius: BorderRadius.circular(6),
+                                          child: Image.asset(
+                                            data[index].image,
+                                            height: 57 * h,
+                                            width: 57 * h,
+                                            fit: BoxFit.cover,
                                           ),
                                         ),
                                       ),
-                                      color: AppColor.blue,
                                     ),
-                                  ),
-                                  SizedBox(
-                                    height: 10 * h,
-                                  ),
-                                  Text(
-                                    "Add Story",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.w400,
-                                      fontSize: 13 * h,
-                                      fontFamily: AppColor.fontNunitoSans,
-                                      height: 24 / 13 * h,
-                                      color: AppColor.dark,
+                                    SizedBox(
+                                      height: 10 * h,
                                     ),
-                                    maxLines: 1,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                ],
-                              ),
-                            )
-                          : Container(
-                              width: 57 * h,
-                              margin: EdgeInsets.only(
-                                right: 25 * w,
-                              ),
-                              child: Column(
-                                children: [
-                                  SizedBox(
-                                    height: 57 * h,
-                                    child: Container(
-                                      height: 57 * h,
-                                      width: 57 * h,
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(9),
-                                        border: Border.all(
-                                          color: AppColor.blue,
-                                          width: 3,
-                                        ),
+                                    Text(
+                                      data[index].text,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w400,
+                                        fontSize: 13 * h,
+                                        fontFamily: AppColor.fontNunitoSans,
+                                        height: 24 / 13 * h,
+                                        color: AppColor.dark,
                                       ),
-                                      child: ClipRRect(
-                                        borderRadius: BorderRadius.circular(6),
-                                        child: Image.asset(
-                                          data[index].image,
-                                          height: 57 * h,
-                                          width: 57 * h,
-                                          fit: BoxFit.cover,
-                                        ),
-                                      ),
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
                                     ),
-                                  ),
-                                  SizedBox(
-                                    height: 10 * h,
-                                  ),
-                                  Text(
-                                    data[index].text,
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.w400,
-                                      fontSize: 13 * h,
-                                      fontFamily: AppColor.fontNunitoSans,
-                                      height: 24 / 13 * h,
-                                      color: AppColor.dark,
-                                    ),
-                                    maxLines: 1,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
-                            ),
-                    ),
-                  ],
-                );
-              },
+                      ),
+                    ],
+                  );
+                },
+              ),
             ),
           ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: 3,
-                itemBuilder: (context, index) {
-              return Container();
-            }),
-          )
+          // ListView.builder(
+          //   itemCount: 3,
+          //     itemBuilder: (context, index) {
+          //   return Container();
+          // })
         ],
       ),
     );
